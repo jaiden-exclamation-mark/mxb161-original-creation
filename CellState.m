81224312743a9bc9e3d400cc1935e32a80b4e13e
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2026-05-25 08:42:56 -0700

    Add comments to `CellState.m`

--- CellState.m
+++ CellState.m
@@ -3,9 +3,10 @@ classdef CellState < uint32
         Uninitialised (0)
         
         % As per A. Alexandridis, et. al (p.194)
-        NoFuel (1)
-        NotIgnited (2)
-        Burning (3)
-        BurnedDown (4)
+  
+        NoFuel (1)     % This cell has no forest fuel. Can represent: rural areas with no vegetation, cities. Cannot burn.
+        NotIgnited (2) % This cell contains fuel but hasn't ignited yet.
+        Burning (3)    % This cell contains fuel that is burning.
+        BurnedDown (4) % This cell conatined fuel that has burned down.
     end
 end
