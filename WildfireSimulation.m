18cd42ea228a3b3158b43ee7565a8a72ab84cabc
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2026-05-25 03:06:45 -0700

    Write basic `WildfireSimulation.get_ignition_probability()`

--- WildfireSimulation.m
+++ WildfireSimulation.m
@@ -1,14 +1,14 @@
 classdef WildfireSimulation
     properties
         % Scalar attributes
-        constant_ignition_probability % p_h (A. Alexandridis, et. al, p. 195)
+        constant_ignition_probability {mustBeBetween(constant_ignition_probability, 0, 1 )} = 0 % p_h (A. Alexandridis, et. al, p. 195)
         wind_speed
         wind_direction
         current_generation % Generation number of the simulation
 
         % Matrix attributes
-        state {mustBeMatrix, mustBeUnderlyingType(state, "CellState")}        % Matrix of cell state enums
-        vegetation {mustBeMatrix, mustBeUnderlyingType(vegetation, "double")} % Matrix of vegetation ignition probabilities
+        state {mustBeMatrix, mustBeUnderlyingType(state, "uint32")} = uint32([]) % Matrix of cell state enums
+        vegetation {mustBeMatrix, mustBeBetween(vegetation, 0, 1)} = []          % Matrix of vegetation ignition probabilities
     end
     methods
         function obj = WildfireSimulation(state, vegetation)
@@ -20,16 +20,24 @@ classdef WildfireSimulation
                 error("Improper number of arguments given to WildfireSimulation constructor.");
             end
             obj.current_generation = 0;
-
+            
             obj.state = state;
             obj.vegetation = vegetation;
         end
-
+        
         function step(obj)
             % Step forward in the cellular automata
         end
         function plot(obj)
             % Plot given state matrix
+        end
+    end
+    methods (Access = private)
+        function probability = get_ignition_probability(obj, row, column)
+            probability = obj.constant_ignition_probability;
+            probability = probability + (1 + obj.vegetation(row, column));
+            wind_probability = 0;
+            probability = probability + wind_probability;
         end
     end
 end
