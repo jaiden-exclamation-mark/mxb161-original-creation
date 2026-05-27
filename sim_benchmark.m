d685774047d80fc52a4468b40f8f40521349b544
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2026-05-27 00:36:47 -0700

    Create benchmark for simulation optimisation testing

---
+++ sim_benchmark.m
@@ -0,0 +1,27 @@
+num_steps = 100;
+
+sim = WildfireSimulation(CellState(2 * ones(100, 100)), -0.5 * ones(100, 100));
+sim.constant_ignition_probability = 0.75;
+sim.continued_burn_probability = 0.5;
+
+sim.state(40:60, 40:60) = CellState.NoFuel;
+sim.state(1, 1) = CellState.Burning;
+sim.plot();
+
+step_time_stats = zeros(1, num_steps);
+current_step_start = inf;
+current_step_end = inf;
+for i = 1:num_steps
+    disp("Starting step " + i + "...");
+    current_step_start = posixtime(datetime());
+    sim = sim.step();
+    current_step_end = posixtime(datetime());
+    step_time_stats(i) = current_step_end - current_step_start;
+    disp("Completed step " + i + " in " + step_time_stats(i) + " seconds!");
+    sim.plot();
+    drawnow;
+end
+
+disp("Mean step time: " + mean(step_time_stats) + "s");
+disp("Median step time: " + median(step_time_stats) + "s");
+disp("Step time range: " + min(step_time_stats) + "-" + max(step_time_stats) + "s");
