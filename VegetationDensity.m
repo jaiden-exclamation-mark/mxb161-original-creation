8b2f63eb5fec1c1a00ef0b697a4c9396c3722101
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2026-05-28 22:08:44 -0700

    Add density enum as per article

---
+++ VegetationDensity.m
@@ -0,0 +1,8 @@
+classdef VegetationDensity < double
+    % Values derived from Alexandris, A. et. al (2008), Table 2.
+    enumeration
+        Sparse (-0.4)
+        Normal (0)
+        Dense (0.3)
+    end
+end
